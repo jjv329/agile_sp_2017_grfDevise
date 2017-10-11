7db544a871074b6c5a59fe126902fd01b6beddcf
Jason Fellows <jfellows@student.cvtc.edu>
2017-10-11 19:18:00 -0400

    repaired event_list_test

--- app/models/event_list.rb
+++ app/models/event_list.rb
@@ -1,7 +1,7 @@
 class EventList < ActiveRecord::Base
   belongs_to :user
 	has_one :user_vehicle
-  validates_presence_of :eventName, :eventDescription, :streetAddress, :City, :State, :Zip,:user_id
+  validates_presence_of :eventName, :eventDescription, :streetAddress, :City, :State, :Zip, :nbrOfRiders, :PUstreetAddress, :PUCity, :PUState, :PUZip, :user_id, :vehicle_id
 
   def self.find_all_by_query(query)
   query = query.downcase
