3d861307282381c9a543162e820376687a6bc019
Luke M. Johnson <ljohnson198@student.cvtc.edu>
2017-10-12 22:33:36 -0500

    Added rideRequest model/table

--- test/models/ride_request_test.rb
+++ test/models/ride_request_test.rb
@@ -4,4 +4,7 @@ class RideRequestTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+	# things like- being invalid withuot a certain column or being able to return something with a certain data type - okay...
+
 end
