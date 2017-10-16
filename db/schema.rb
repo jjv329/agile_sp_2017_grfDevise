c807fdb53feafda7b9f42dc5fe3225e93ebd16b3
Luke M. Johnson <ljohnson198@student.cvtc.edu>
2017-10-15 21:20:42 -0500

    More work on the Ride Request Controller test - pasted in the version of profiles_controller_test.rb from master as part of troubleshooting.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20171014182645) do
 
   create_table "event_lists", force: :cascade do |t|
@@ -51,7 +50,6 @@ ActiveRecord::Schema.define(version: 20171014182645) do
     t.string   "acct_type"
     t.string   "phone_number"
   end
-
 
   create_table "ride_requests", force: :cascade do |t|
     t.integer  "user_id"
--- test/controllers/ride_requests_controller_test.rb
+++ test/controllers/ride_requests_controller_test.rb
@@ -2,11 +2,16 @@ require 'test_helper'
 
 class RideRequestsControllerTest < ActionController::TestCase
 
+  setup do
+    @ride_request = ride_requests(:one)
+    sign_in users(:user_one)
+    sign_in users(:user_two)
+  end
   
   test "Should be able to pull the index page of this Controller" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:ride_requests)
+    assert_not_nil assigns(:ride_request)
   end
 
   test "Should be able to pull the new page of this Controller" do
@@ -14,8 +19,11 @@ class RideRequestsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-
-  
-
+  test "Create a new ride_requests row, should succeed" do
+    assert_difference('RideRequest.count') do
+    	post :create, ride_request: { user_id: @ride_request.user_id, event_id: @ride_request.event_id, confirmed: @ride_request.confirmed }
+    end
+    assert_redirected_to ride_request_path(assigns(:ride_request))
+  end
 
 end
